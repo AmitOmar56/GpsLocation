69a3c3e217fdf7cab5da6c20bca9b2db15d7cbed
AmitOmar56 <40773012+AmitOmar56@users.noreply.github.com>
2018-07-03 01:58:05 +0530

    Initial commit

---
+++ app/src/main/java/com/example/lifeplaytrip/gpslocation/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.lifeplaytrip.gpslocation;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        requestPermissionsIfNotGranted();
+    }
+
+    private void requestPermissionsIfNotGranted() {
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
+        }
+    }
+
+    public void start(View view) {
+        startService(new Intent(MainActivity.this, MyService.class));
+
+    }
+
+    public void stop(View view) {
+        stopService(new Intent(MainActivity.this, MyService.class));
+    }
+}
